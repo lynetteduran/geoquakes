e373edc142ce42878dc96feb972d565c359d41ab
lynetteduran <lynetteduran@Lynettes-MacBook-Air.local>
2016-09-14 23:10:02 -0700

    implemented changes as per assignment instructions, except adding map markers

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,22 @@
-// define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var weeklyQuakesEndpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).on("ready", function() {
+	$.get(weeklyQuakesEndpoint, function(responseData) {
+		var passEarthquakes = responseData.features;
+		
+		var source = $('#earthquakeTitlesDisplay').html();
+		var template = Handlebars.compile(source);
+		var quakeHtml = template({ earthquake : passEarthquakes });
+		
 
-  // CODE IN HERE!
+		console.log('generated html string:', quakeHtml);
+		$("#earthquakeTitles").append(quakeHtml);
+	});
 
+   //  var marker = new google.maps.Marker({
+   //  	forEach()
+   //  position: myLatLng,
+   //  map: map,
+   //  title: 'Hello World!'
+  	// });
 });
